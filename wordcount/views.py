ef3fd2a5da34a8f74d7a5631da3b089ae4c5a387
jiyouth <wldbs1417123@naver.com>
2020-04-16 00:03:46 +0900

    gitignore

--- wordcount/views.py
+++ wordcount/views.py
@@ -28,3 +28,18 @@ def count(request):
 
     return render(request, 'wordcount/count.html', {'fulltext' : full_text, 'total': len(word_list), 'dictionary': word_dictionary.items()})
 
+    #공백포함    
+def blank(request):    
+    text = request.GET['fulltext']
+    words = text.split()
+    word_dictionary = {}
+    for word in words:
+        if word in word_dictionary:
+            #increase
+            word_dictionary[word]+=1
+            # add to dictionary
+            word_dictionary[word]=1
+    total = len(words)*2 -1
+
+    return render(request, 'result.html', {'full': text, 'total' : total, 'dictionary' : word_dictionary.items()})
+
